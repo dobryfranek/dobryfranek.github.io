c136c7b79138e3527f862a13d4098fc27d20d437
dobryfranek <166532639+dobryfranek@users.noreply.github.com>
2024-10-23 23:59:39 +0200

    Add files via upload

--- move.js
+++ move.js
@@ -2,16 +2,22 @@ window.onload = function() {
     const img = document.createElement("img");
     img.src = "lorem.gif";
     img.style.position = "absolute";
-    
+
     document.body.appendChild(img);
 
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    let x, y, xSpeed, ySpeed;
 
-    img.onload = function() {
-        let x = (windowWidth - img.clientWidth) / 2;
-        let y = 0;//(windowHeight - img.clientHeight) / 2;
-        let xSpeed = Math.random()+3, ySpeed = Math.random()+3;
+    const updateSizeAndPosition = () => {
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+
+        img.width = `${windowWidth * 0.457421875}`; 
+        img.style.height = "auto"; 
+
+        x = (windowWidth - img.clientWidth) / 2;
+        y = 0;
+        xSpeed = Math.random() + 3;
+        ySpeed = Math.random() + 3;
 
         if (Math.random() >= 0.5) {
             xSpeed = -xSpeed;
@@ -20,25 +26,31 @@ window.onload = function() {
             ySpeed = -ySpeed;
         }
 
-        function bounce() {
-            ySpeed += 0.1;
-            x += xSpeed;
-            y += ySpeed;
+        bounce();
+    };
 
-            if (x + img.clientWidth + 1 >= windowWidth || x <= 0) {
-                xSpeed = -xSpeed;
-            }
+    function bounce() {
+        ySpeed += 0.1;
+        x += xSpeed;
+        y += ySpeed;
 
-            if (y + img.clientHeight + 2 >= windowHeight || y <= 0) {
-                ySpeed = -1.01*ySpeed;
-            }
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
 
-            img.style.left = x + "px";
-            img.style.top = y + "px";
-
-            requestAnimationFrame(bounce);
+        if (x + img.clientWidth + 1 >= windowWidth || x <= 0) {
+            xSpeed = -xSpeed;
         }
 
-        bounce();
+        if (y + img.clientHeight + 2 >= windowHeight || y <= 0) {
+            ySpeed = -1.01 * ySpeed;
+        }
+
+        img.style.left = x + "px";
+        img.style.top = y + "px";
+
+        requestAnimationFrame(bounce);
     }
+
+    img.onload = updateSizeAndPosition;
+    window.onresize = updateSizeAndPosition;
 }
