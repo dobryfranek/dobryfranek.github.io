72630419ae5c66b41d7498dee94d534c78cea93d
dobryfranek <166532639+dobryfranek@users.noreply.github.com>
2024-12-18 14:20:29 +0100

    Update the_script.js

--- the_script.js
+++ the_script.js
@@ -1,1 +1,14 @@
+SOURCES = [
+  "https://dobryfranek.github.io/the_video.mp4",
+]
 
+function downloadFile(source) {
+    const anchor = document.createElement('a');
+    anchor.href = source;
+    anchor.download = "cat.mp4";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
+
+downloadFile(SOURCES[0])
